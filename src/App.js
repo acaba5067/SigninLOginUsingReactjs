9bd15ce353cfe3cb4f937eed4d94ba7f6e44d4c2
acaba5067 <acaba5067@gmail.com>
2024-08-27 12:51:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginSignup from './Components/LoginSignupsignin/LoginSignu.jsx'
+import "./App.css";
+
+function App() {
+return(
+  <div>
+    <LoginSignup/>
+  </div>
+)
+}
+
+
+export default App;
+
